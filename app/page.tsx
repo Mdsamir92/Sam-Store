768380ed1b11dadc350ef12b0e4ff929de9a6835
Mohammad Samir <87142605+Mdsamir92@users.noreply.github.com>
2026-02-04 12:14:37 +0530

    Replace Hero component with HeroCarousel and add Suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,21 @@
-import Hero from "@/app/components/Hero";
+import HeroCarousel from "@/app/components/Hero";
 import ProductList from "@/app/components/ProductList";
+import { Suspense } from "react";
 
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export default function Home() {
   return (
-   <div >
- 
-   <Hero/>
-<ProductList/>
+    <div>
+      <HeroCarousel />
 
-   </div>
+      <Suspense
+        fallback={<div className="py-10 text-center">Loading products...</div>}
+      >
+        <ProductList />
+      </Suspense>
+    </div>
   );
 }
